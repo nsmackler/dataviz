3d1d25fdf97581fb4c6cd308996764dd2c548ba6
Noah Snyder-Mackler <nsmack@gmail.com>
2019-04-03 15:51:30 -0700

    Update napoleon.R

--- napoleon/napoleon.R
+++ napoleon/napoleon.R
@@ -1,11 +1,11 @@
-## Load in data
-troops <- read.table("https://raw.githubusercontent.com/nsmackler/PSYCH-531/master/napoleon/troops.txt", header=T)
-cities <- read.table("https://raw.githubusercontent.com/nsmackler/PSYCH-531/master/napoleon/cities.txt", header=T)
-
 ## load in packages (just tidyverse)
 library(tidyverse)
 
-troops %>% head()
+## Load in data
+troops <- read_delim("https://raw.githubusercontent.com/nsmackler/PSYCH-531/master/napoleon/troops.txt", delim=" ", col_names = TRUE)
+cities <- read_delim("https://raw.githubusercontent.com/nsmackler/PSYCH-531/master/napoleon/cities.txt", delim=" ", col_names = TRUE)
+
+troops
 
 ## plot with just paths
 ggplot(troops, aes(x = long, y = lat, group = group)) +
